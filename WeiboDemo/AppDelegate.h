1512f286efd87cabd3feda11e918f621835c4f97
ten-step.pengbo <bo.peng@ten-step.com>
2016-01-08 16:14:52 +0800

    Initial Commit

---
+++ WeiboDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiboDemo
+//
+//  Created by ten-step on 16/1/8.
+//  Copyright © 2016年 ten-step. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
